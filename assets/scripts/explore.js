286cc0dd4afa76b5572e9644028acf316ed83bb2
bpanuco11 <brandonpanuco2270@gmail.com>
2024-05-04 00:22:17 -0700

    finished explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const voiceSelect = document.getElementById('voice-select');
+  let isSpeaking = false;
   
   // check if user's browser supports speechSynthesis API, else throw error on console
   if ('speechSynthesis' in window) {
@@ -28,6 +29,32 @@ function init() {
 
     // New objects may not load immediately when the page is loaded, so manually force it
     speechSynth.onvoiceschanged();
+
+    // Event listener for "Press to Talk" button
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+      if (!isSpeaking) {
+        const textToSpeak = document.getElementById('text-to-speak').value;
+        const selectedVoice = voiceSelect.value;
+        console.log(selectedVoice);
+        // object stores the text that needs to be spoken
+        const toBeSpoken = new SpeechSynthesisUtterance(textToSpeak);
+        // find voice in the list that matches selected voice by the user
+        const voice = speechSynth.getVoices().find(voice => voice.name === selectedVoice);
+        // add voice to the toBeSpoken object
+        toBeSpoken.voice = voice;
+        isSpeaking = true;
+        document.querySelector('img').src = 'assets/images/smiling-open.png';
+        // runs  asynchronous so it breaks whenever isSpeaking turns to false!
+        toBeSpoken.onend = () => {
+          isSpeaking = false;
+          document.querySelector('img').src = 'assets/images/smiling.png';
+        };
+        speechSynth.speak(toBeSpoken);
+        //console.log(document.querySelector('img').src)
+        //console.log(isSpeaking)
+      }
+    });
   } 
   else 
     console.error('Error, SpeechSynthesis not supported by your Browser');
