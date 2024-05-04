9120a2a13226029f1780ee8e87d9b1eef125178f
bpanuco11 <brandonpanuco2270@gmail.com>
2024-05-03 23:43:04 -0700

    Updated explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,33 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  
+  // check if user's browser supports speechSynthesis API, else throw error on console
+  if ('speechSynthesis' in window) {
+
+    // create speechSynthesis object
+    const speechSynth = window.speechSynthesis;
+
+    // update voices list when adding or removing voices in the list
+    speechSynth.onvoiceschanged = () => {
+      // Clear existing options
+      voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+      
+      // Get the list of available voices from the user's browser
+      const voices = speechSynth.getVoices();
+
+      // Populate dropdown with voices
+      voices.forEach(voice => {
+        const option = new Option(`${voice.name} (${voice.lang})`, voice.name);
+        voiceSelect.appendChild(option);
+      });
+    };
+
+    // New objects may not load immediately when the page is loaded, so manually force it
+    speechSynth.onvoiceschanged();
+  } 
+  else 
+    console.error('Error, SpeechSynthesis not supported by your Browser');
+  
 }
